d216d0917208ae61ab980b8b4fbee25d5d6982fa
Frully <xxfrully@gmail.com>
2013-06-18 15:27:09 +0800

    changed loadObject() to sequentially insert collection

--- mongoose_fixtures.js
+++ mongoose_fixtures.js
@@ -121,7 +121,7 @@ function loadObject(data, db, callback) {
     var iterator = function(modelName, next){
         insertCollection(modelName, data[modelName], db, next);
     };
-    async.forEach(Object.keys(data), iterator, callback);
+    async.forEachSeries(Object.keys(data), iterator, callback);
 }
 
 
